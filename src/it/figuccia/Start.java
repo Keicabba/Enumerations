745fd1b40f028da8c836209f33f87d6e17d5b441
Keicabba <peppino_cbr@hotmail.com>
2023-02-25 11:35:14 +0100

    effettuata modifica richiesta di classe enum separata

--- src/it/figuccia/Start.java
+++ src/it/figuccia/Start.java
@@ -2,27 +2,27 @@ package it.figuccia;
 
 public class Start {
     public static void main(String[] args) {
-        enum Month {
-            January,
-            February,
-            March,
-            April,
-            May,
-            June,
-            July,
-            August,
-            September,
-            October,
-            November,
-            December
-        }
-
         for (Month month : Month.values()) {
-            if (month.name().endsWith("y")) {
+            char capitalLetter = month.name().charAt(0);
+            String word = month.name().substring(1).toLowerCase();
+            String month1 = capitalLetter + word;
+
+            if (month1.endsWith("y")) {
+                System.out.println(month1 + " ends with y");
+            }
+            else {System.out.println(month1 + " doesn't end with y");
+            }
+        }
+    }
+}
+/* Bastava questo commentato sotto. Ma i mesi in maiuscolo mi davano fastidio e li ho rieditati.
+    public static void main(String[] args) {
+        for (Month month : Month.values()) {
+            if (month.name().endsWith("Y")) {
                 System.out.println(month.name() + " ends with y");
             } else {
                 System.out.println(month.name() + " doesn't end with y");
             }
         }
     }
-}
+*/
